36e2399b815cecdcf629f297027dd3761e6ee982
EvilMangos <jchamets@gmail.com>
2024-08-19 23:00:57 -0400

    MKS-5_my_experience: my experience mobile animations

--- src/components/MyExperience/Slider/Description/Description.tsx
+++ src/components/MyExperience/Slider/Description/Description.tsx
@@ -1,12 +1,22 @@
 import classes from "./Description.module.scss";
 import classNames from "classnames";
+import { useEffect, useRef } from "react";
 
 const Description = ({ text, outClass }) => {
+	const textRef = useRef(null);
 	const containerClasses = classNames(classes.container, outClass);
+	useEffect(() => {
+		if (textRef && textRef.current) {
+			const clone = textRef.current.cloneNode(true)
+			clone.innerHTML = text;
+			textRef.current.parentNode.replaceChild(clone, textRef.current);
+			textRef.current = clone;
+		}
+	}, [text]);
 	return (
 		<div className={containerClasses}>
 			<h3 className={classes.title}>Description</h3>
-			<div className={classes.text}>
+			<div className={classes.text} ref={textRef}>
 				<div className={classes.textTransparency}>
 					{text}
 				</div>
